1cb88a0b5bdf67f3c33b66985ba14725e622fcef
James Eastham <dev@jameseastham.co.uk>
2023-04-02 19:53:32 +0100

    Update response

--- src/AppRuner.Api/Controllers/WeatherForecastController.cs
+++ src/AppRuner.Api/Controllers/WeatherForecastController.cs
@@ -25,7 +25,8 @@ public class WeatherForecastController : ControllerBase
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+            Test = Guid.NewGuid().ToString()
         })
         .ToArray();
     }
