68263b86fd4f6b2b52ce352f2dd6c55483f74ae7
James Eastham <dev@jameseastham.co.uk>
2023-04-07 12:35:52 +0100

    Update to single-AZ. Cost saving

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,9 @@ module "vpc" {
   name = "main"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["eu-west-1a"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
@@ -78,7 +78,6 @@ resource "aws_security_group" "app_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
 
 resource "aws_apprunner_vpc_connector" "connector" {
   vpc_connector_name = "vpc-connector"
